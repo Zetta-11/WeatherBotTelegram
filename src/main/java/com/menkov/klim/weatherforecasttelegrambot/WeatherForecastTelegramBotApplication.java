adf72c8e89fbac4840d245ab7a24b22a759a3216
Klim Menkov <klim.menkov@ukr.net>
2023-06-19 16:56:17 +0300

    Initial commit

---
+++ src/main/java/com/menkov/klim/weatherforecasttelegrambot/WeatherForecastTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.menkov.klim.weatherforecasttelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherForecastTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherForecastTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/menkov/klim/weatherforecasttelegrambot/WeatherForecastTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menkov.klim.weatherforecasttelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherForecastTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
